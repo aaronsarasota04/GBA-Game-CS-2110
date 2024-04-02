3815e7c9198b4495fd81081784060f7ca9b80aed
Aaron Rahim <rahimabdur1128@gmail.com>
2024-04-02 12:43:10 -0400

    A soccer game that will run on the GBA

---
+++ README.txt
@@ -0,0 +1,4 @@
+A soccer game, you must score 3 goals to win. If the ball goes out of the screen or hits the red line then you automatically lose.
+Press Enter to start
+Control the ball using up down left right buttons
+You can go back to the main screen any time by pressing back space.
---
+++ images/goal.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=40x30 goal goal.png 
+ * Time-stamp: Thursday 03/28/2024, 19:25:10
+ * 
+ * Image Information
+ * -----------------
+ * goal.png 40@30
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef GOAL_H
+#define GOAL_H
+
+extern const unsigned short goal[1200];
+#define GOAL_SIZE 2400
+#define GOAL_LENGTH 1200
+#define GOAL_WIDTH 40
+#define GOAL_HEIGHT 30
+
+#endif
+
